9d9a70213a050fcb6ffba769ccc06d5100c9aac0
rustleupchef <rustle.up.chef@gmail.com>
2025-09-21 06:08:06 -0500

    changed offset

--- static/index/script.js
+++ static/index/script.js
@@ -9,7 +9,7 @@ window.onload = function() {
     
         window.addEventListener("mousemove", function(e) {
             const rect = canvas.getBoundingClientRect();
-            const x = e.pageX;
+            const x = e.pageX - rect.left;
             const y = e.pageY - rect.top;
             canvas.draw(texture).swirl(x, y, 200, 4).update();
         });
