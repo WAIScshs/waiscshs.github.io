5d71aa7060c87dd19e81eadebde3aae910edfd93
rustleupchef <rustle.up.chef@gmail.com>
2025-09-28 18:55:30 -0500

    removed background from other pages

--- static/meetings/script.js
+++ static/meetings/script.js
@@ -17,23 +17,7 @@ function clamp(num, min, max) {
 }
 
 function loadSizes() {
-    let vector1 = document.getElementById("vector1");
-    const vector2 = document.getElementById("vector2");
     const content = document.querySelector(".content");
-
-    if (vector1 === null) {
-        vector1 = document.querySelectorAll("div canvas")[0];
-    }
-
-    vector1.style.width = currentWidth * 1.5 + "px";
-    vector1.style.height = currentHeight + "px";
-    vector1.style.left = -(currentWidth * 1.5 - window.innerWidth)/2 + "px";
-    vector1.style.bottom = -currentHeight * 0.25 + "px";
-
-    vector2.style.width = currentWidth * 1.6 + "px";
-    vector2.style.height = currentHeight + "px";
-    vector2.style.left = -(currentWidth * 1.6 - window.innerWidth)/2 + "px";
-    vector2.style.bottom = -currentHeight * 0.43 + "px";
 
     content.style.width = clamp(currentWidth * 0.625, 0, window.innerWidth) + "px";
     content.style.height = clamp(currentHeight * 0.625, 0, window.innerHeight) + "px";
@@ -67,30 +51,6 @@ window.onload = function() {
     });
 
     setTimeout(loadSizes, 100);
-
-    let mouseX = 0;
-    let mouseY = 0;
-
-    const image = document.getElementById("vector1");
-    var canvas = fx.canvas();
-    var texture = canvas.texture(image);
-    
-    image.parentNode.insertBefore(canvas, image);
-    image.parentNode.removeChild(image);
-    canvas.draw(texture).update();
-    
-    window.addEventListener("mousemove", function(e) {
-        const rect = canvas.getBoundingClientRect();
-        mouseX = e.pageX - rect.left;
-        mouseY = e.pageY - rect.top;
-
-        mouseX = ((mouseX - canvas.width/2)/2) + canvas.width/2;
-        mouseY = ((mouseY - canvas.height/2)/2) + canvas.height/2;
-
-        canvas.draw(texture)
-            .zoomBlur(parseInt(mouseX), parseInt(mouseY), 0.2)
-            .update();
-    });
 
     const today = new Date();
     const firstMeeting = new Date("2025-09-18");
